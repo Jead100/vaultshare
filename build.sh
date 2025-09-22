2173ef5b593a1522e42e75090e69bd56dd025bae
Jordan <jordan_account2@outlook.com>
2025-09-22 12:59:26 -0400

    chore(deploy): prebuild Tailwind CSS locally and commit dist output

--- build.sh
+++ build.sh
@@ -3,10 +3,6 @@ set -o errexit
 # Python deps
 pip install -r requirements.txt
 
-# Tailwind (django-tailwind handles Node deps for the 'theme' app)
-python manage.py tailwind install
-python manage.py tailwind build
-
 # Django static & DB
 python manage.py collectstatic --no-input
 python manage.py migrate
