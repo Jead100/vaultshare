6e15d7c9ec60030492ec22b181d514dbc37b8122
Jordan <jordan_account2@outlook.com>
2026-01-04 14:01:33 -0500

    refactor(auth): remove redundant SimpleJWT overrides
    - Simplifies auth setup by relying on SimpleJWT defaults.
    - Improves URL organization and readability.

--- apps/users/jwt_serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-
-class EmailTokenObtainPairSerializer(TokenObtainPairSerializer):
-    # Tell SimpleJWT that our login identifier is the `email` field
-    username_field = "email"
--- apps/users/jwt_views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-from .jwt_serializers import EmailTokenObtainPairSerializer
-
-
-class EmailTokenObtainPairView(TokenObtainPairView):
-    serializer_class = EmailTokenObtainPairSerializer
