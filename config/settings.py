33d5673dbd3928a800de19e6d598660c2eeea976
Jordan <jordan_account2@outlook.com>
2026-01-04 11:43:10 -0500

    feat(config): decouple database selection from DEBUG

--- config/settings.py
+++ config/settings.py
@@ -98,23 +98,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 
-DATABASE_URL = config("DATABASE_URL", default="")
+DATABASE_URL = config("DATABASE_URL", default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
 
-if DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=True,
-        )
-    }
+DATABASES = {
+    "default": dj_database_url.parse(
+        DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=DATABASE_URL.startswith(("postgres://", "postgresql://")),
+    )
+}
 
 
 # Password validation
